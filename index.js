54fc5099fd37b3312e809d687f9e78c46407290a
desarrollo.net.cardenas@hotmail.com <desarrollo.net.cardenas@hotmail.com>
2022-03-19 23:56:39 -0700

    Test

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const {mssqlConnector} = require('./repository/mssqlv2');
 
 const connector = mssqlConnector();
-connector.exec('select * from myDatabase2;')
+connector.exec('select * from myDatabase;')
